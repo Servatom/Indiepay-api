2b21a6f4884e1f2aad5d50cc2aedffbac1d320df
Yashvardhan Arora <yash22arora@gmail.com>
2024-07-10 02:16:29 +0530

    Create Project services, S3 upload utility func

---
+++ server/src/api/services/project.ts
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+import { IProject, Project } from "../models/project";
+import { create } from "domain";
+
+export const projectService = {
+  createProject: async (project: IProject): Promise<IProject | unknown> => {
+    try {
+      const newProject = new Project({
+        _id: new mongoose.Types.ObjectId(),
+        name: project.name,
+        ownerID: project.ownerID,
+        webhookURL: project.webhookURL,
+        callbackURL: project.callbackURL,
+        vpa: project.vpa,
+        totalRevenue: 0,
+        QRImageS3URL: project.QRImageS3URL,
+      });
+
+      const result: IProject = await newProject.save();
+      return result;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  },
+
+  getProject: async (projectID: string): Promise<IProject | null> => {
+    try {
+      const result = await Project.findOne({ _id: projectID });
+      return result;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  },
+
+  getProjectsByOwnerID: async (ownerID: string): Promise<IProject[] | null> => {
+    try {
+      const result = await Project.find({ ownerID: ownerID });
+      return result;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  },
+
+  updateProject: async (
+    projectID: string,
+    project: IProject
+  ): Promise<IProject | null> => {
+    try {
+      const result = await Project.findOneAndUpdate(
+        { _id: projectID },
+        project,
+        {
+          new: true,
+        }
+      );
+      return result;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  },
+
+  deleteProject: async (projectID: string): Promise<boolean> => {
+    try {
+      await Project.deleteOne({
+        _id: projectID,
+      });
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  },
+};
--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -1,4 +1,5 @@
 require("dotenv").config();
+import AWS from "aws-sdk";
 
 import crypto from "crypto";
 
@@ -36,4 +37,27 @@ function decrypt(text: string) {
   return decrypted.toString();
 }
 
-export { encrypt, decrypt };
+const uploadImageOnS3 = async (file: any, key: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const s3 = new AWS.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+    });
+
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME!,
+      Key: key,
+      Body: file.buffer,
+    };
+
+    s3.upload(params, (err: any, data: any) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data.Location);
+    });
+  });
+};
+
+export { encrypt, decrypt, uploadImageOnS3 };
